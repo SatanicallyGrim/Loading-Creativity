98f848660fae11bb5c4cfd2b0c4c83948eaf29a7
SatanicallyGrim <61483076+SatanicallyGrim@users.noreply.github.com>
2021-02-19 21:51:40 +1100

    initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    private float p1Speed = 3f;
+    public float jmpHeight = 3f;
+    private bool p1jumping = false;
+    private Rigidbody2D rb;
+    #endregion
+    private void Awake()
+    {
+        // calls the Rigidbody2D before the first frame
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Moves and object to the right multiplied by the player's move speed
+        transform.Translate(Vector2.right * Time.deltaTime * p1Speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && p1jumping == false)
+        {
+            //Allows the player to jump multiplied by the player's jump height 
+            rb.velocity = (Vector2.up * jmpHeight);
+            // once the player has jumped set player's jump to true
+            p1jumping = true;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        // detects if an object has a tag called Ground
+        if (col.gameObject.tag == "Ground")
+        {
+            //if player is colliding with ground tag player's jum is set to false
+            p1jumping = false;
+        }
+    }
+}
